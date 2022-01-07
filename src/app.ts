bb63abf28a51610691af7ed631a8986af4086c9e
mikroby <rtamas@hotmail.com>
2022-01-07 18:46:22 +0100

    solved

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { TransformerHero } from './transformer';
+import { HumanHero } from './hero';
 // Importáld be a HumanHero és TransformerHero osztályokat.
 
 
@@ -8,7 +10,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+  { id: 12, name: "Hercules", sex: "male", age: 32, health: 95 },
+  { id: 10, name: "Spider Man", sex: "male", age: 30, health: 85 },
+  { id: 20, name: "James Bond", sex: "male", age: 35, health: 90 },
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +23,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+  { id: 1, name: "IronHide", wings: 2, wheels: 5, clan: "metal" },
+  { id: 2, name: "Cyborg", wings: 1, wheels: 3, clan: "bio" },
+  { id: 3, name: "Brainstrom", wings: 0, wheels: 3, clan: "robots" },
+];
